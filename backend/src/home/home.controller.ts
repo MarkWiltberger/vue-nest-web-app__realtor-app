947d0c909a01a4e071c228b807a5df1d14e09326
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-04-06 09:02:55 -0400

    Checkin of nestjs realtor-app to vue-nest base scaffolding backend

---
+++ backend/src/home/home.controller.ts
@@ -0,0 +1,72 @@
+import {
+  Controller,
+  Body,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Query,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { HomeService } from './home.service';
+import { CreateHomeDto, HomeResponseDTO, UpdateHomeDto } from './dto/home.dto';
+import { PropertyType } from '@prisma/client';
+import { User } from 'src/user/decorators/user.decorator';
+
+@Controller('home')
+export class HomeController {
+
+  constructor(private readonly homeService: HomeService) {}
+
+  @Get('/')
+  getHomes(
+    @Query('city') city?: string,
+    @Query('minPrice') minPrice?: string,
+    @Query('maxPrice') maxPrice?: string,
+    @Query('propertyType') property_type?: PropertyType,
+  ): Promise<HomeResponseDTO[]> {
+    const price =
+      minPrice || maxPrice
+        ? {
+            ...(minPrice && { gte: parseFloat(minPrice) }),
+            ...(maxPrice && { lte: parseFloat(maxPrice) }),
+          }
+        : undefined;
+
+    const filters = {
+      ...(city && { city }),
+      ...(price && { price }),
+      ...(property_type && { property_type }),
+    };
+    return this.homeService.getHomes(filters);
+  }
+
+  @Get(':id')
+  getHome(@Param('id', ParseIntPipe) id: number) {
+    return this.homeService.getHomeById(id);
+  }
+
+  @Post()
+  createHome(@Body() newHome: CreateHomeDto, @User() user) {
+    return user;
+    // return this.homeService.createHome(newHome);
+  }
+
+  @Put(':id')
+  updateHome(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateHomeDto,
+  ) {
+    return this.homeService.updateHomeById(id, body);
+  }
+
+  @Delete(':id')
+  deleteHome(@Param('id', ParseIntPipe) id: number) {
+    return this.homeService.deleteHomeById(id);
+  }
+}
+
+// http://localhost:3000/home/Toronto/500000/1500000
+
+// http://localhost:3000/home?city=Toronto&minPrice=500000&maxPrice=1500000
---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect;
+  }
+  async onModuleDestroy() {
+    this.$disconnect();
+  }
+}
