4e5e3ad94d83c302fccd312964e867e5b2f71a66
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-04-10 15:37:14 -0400

    change paths to prisma module to relative paths for vercel production environment to fix runtime 'module not found' error

--- backend/src/home/home.module.ts
+++ backend/src/home/home.module.ts
@@ -1,7 +1,7 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { HomeController } from './home.controller';
 import { HomeService } from './home.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
