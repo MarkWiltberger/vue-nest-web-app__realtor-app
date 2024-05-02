b31a52c41bea2e8dc7c20d17f991ee8c9e23d1e8
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-05-02 12:48:30 -0400

    write test that service calls prisma home.findMany with correct params

--- backend/src/home/home.service.spec.ts
+++ backend/src/home/home.service.spec.ts
@@ -1,18 +1,86 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HomeService } from './HomeService';
+import { PrismaService } from '../prisma/prisma.service';
+import { HomeService } from './home.service';
+import { PropertyType } from '@prisma/client';
+
+const mockGetHomes = [
+  {
+    id: 1,
+    address: '2345 William St',
+    city: 'Toronto',
+    price: 1500000,
+    image: 'img1',
+    numberOfBedrooms: 3,
+    numberOfBathrooms: 2.5,
+    propertyType: PropertyType.RESIDENTIAL,
+    images: [
+      {
+        url: 'src1',
+      },
+    ],
+  },
+];
 
 describe('HomeService', () => {
   let service: HomeService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HomeService],
+      providers: [
+        HomeService,
+        {
+          provide: PrismaService,
+          useValue: {
+            home: {
+              findMany: jest.fn().mockReturnValue(mockGetHomes),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<HomeService>(HomeService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getHomes', () => {
+    const filters = {
+      city: 'Toronto',
+      price: {
+        gte: 1000000,
+        lte: 1500000,
+      },
+      property_type: PropertyType.RESIDENTIAL,
+    };
+
+    it('should call prisma home.findMany with correct params', async () => {
+      const mockPrismaFindManyHomes = jest.fn().mockReturnValue(mockGetHomes);
+
+      jest
+        .spyOn(prismaService.home, 'findMany')
+        .mockImplementation(mockPrismaFindManyHomes);
+
+      await service.getHomes(filters);
+
+      expect(mockPrismaFindManyHomes).toBeCalledWith({
+        select: {
+          id: true,
+          address: true,
+          city: true,
+          price: true,
+          property_type: true,
+          number_of_bedrooms: true,
+          number_of_bathrooms: true,
+          images: {
+            select: {
+              url: true,
+            },
+            take: 1,
+          },
+        },
+        where: filters,
+      });
+    });
   });
 });
