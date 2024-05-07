26828cb489f6c4768550eeac2f34bb4ca2af9092
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-05-07 13:32:12 -0400

    remove console log functions from home.service.ts & user.interceptor.ts

--- backend/src/user/interceptors/user.interceptor.ts
+++ backend/src/user/interceptors/user.interceptor.ts
@@ -3,7 +3,6 @@ import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 export class UserInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
-    console.log(request);
     return handler.handle();
   }
 }
