396517f4d36d171e50d8e11898656dd0694cf54d
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-05-08 09:13:34 -0400

    created user interceptor spec and modified user interceptor

--- backend/src/user/interceptors/user.interceptor.ts
+++ backend/src/user/interceptors/user.interceptor.ts
@@ -1,8 +1,14 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 export class UserInterceptor implements NestInterceptor {
+  /* intercept(context: ExecutionContext, handler: CallHandler) {
+    return handler.handle();
+  } */
+
   intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
+    console.log({ request });
     return handler.handle();
   }
 }
