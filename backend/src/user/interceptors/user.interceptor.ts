52de66520790792bbfae6c91c79c2b680dedb8bf
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-05-28 14:50:43 -0400

    add user interceptor to extract token and add user decorator

--- backend/src/user/interceptors/user.interceptor.ts
+++ backend/src/user/interceptors/user.interceptor.ts
@@ -1,14 +1,12 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import * as jwt from 'jsonwebtoken';
 
 export class UserInterceptor implements NestInterceptor {
-  /* intercept(context: ExecutionContext, handler: CallHandler) {
-    return handler.handle();
-  } */
-
   intercept(context: ExecutionContext, handler: CallHandler) {
     const request = context.switchToHttp().getRequest();
-    console.log({ request });
+    const token = request?.headers?.authorization?.split('Bearer ')[1];
+    const user = jwt.decode(token);
+    request.user = user;
     return handler.handle();
   }
 }
