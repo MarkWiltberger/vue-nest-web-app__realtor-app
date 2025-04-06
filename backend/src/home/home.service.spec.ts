b140115bbfd0f4f33fc22ab29097a6c8042ff0f3
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-04-06 09:01:00 -0400

    add test for create home service for correct payload

--- backend/src/home/home.service.spec.ts
+++ backend/src/home/home.service.spec.ts
@@ -11,14 +11,36 @@ const mockGetHomes = [
     city: 'Toronto',
     price: 1500000,
     image: 'img1',
-    numberOfBedrooms: 3,
-    numberOfBathrooms: 2.5,
-    propertyType: PropertyType.RESIDENTIAL,
+    number_of_bedrooms: 3,
+    number_of_bathrooms: 2.5,
+    property_type: PropertyType.RESIDENTIAL,
     images: [
       {
         url: 'src1',
       },
     ],
+  },
+];
+
+const mockHome = {
+  id: 1,
+  address: '2345 William St',
+  city: 'Toronto',
+  price: 1500000,
+  image: 'img1',
+  number_of_bedrooms: 3,
+  number_of_bathrooms: 2.5,
+  property_type: PropertyType.RESIDENTIAL,
+};
+
+const mockImages = [
+  {
+    id: 1,
+    url: 'src1',
+  },
+  {
+    id: 2,
+    url: 'src2',
   },
 ];
 
@@ -35,6 +57,10 @@ describe('HomeService', () => {
           useValue: {
             home: {
               findMany: jest.fn().mockReturnValue(mockGetHomes),
+              create: jest.fn().mockReturnValue(mockHome),
+            },
+            image: {
+              createMany: jest.fn().mockReturnValue(mockImages),
             },
           },
         },
@@ -94,6 +120,46 @@ describe('HomeService', () => {
       await expect(service.getHomes(filters)).rejects.toThrowError(
         NotFoundException,
       );
+    });
+  });
+
+  describe('createHome', () => {
+    const mockCreateHomeParams = {
+      address: '111 Green Ave',
+      numberOfBedrooms: 2,
+      numberOfBathrooms: 2,
+      city: 'Vancouver',
+      landSize: 4400,
+      price: 3000000,
+      propertyType: PropertyType.RESIDENTIAL,
+      images: [
+        {
+          url: 'src1',
+        },
+      ],
+    };
+
+    it('should call prisma home.create with the correct payload', async () => {
+      const mockCreateHome = jest.fn().mockReturnValue(mockHome);
+
+      jest
+        .spyOn(prismaService.home, 'create')
+        .mockImplementation(mockCreateHome);
+
+      await service.createHome(mockCreateHomeParams, 5);
+
+      expect(mockCreateHome).toBeCalledWith({
+        data: {
+          address: '111 Green Ave',
+          number_of_bedrooms: 2,
+          number_of_bathrooms: 2,
+          city: 'Vancouver',
+          land_size: 4400,
+          price: 3000000,
+          property_type: PropertyType.RESIDENTIAL,
+          realtor_id: 5,
+        },
+      });
     });
   });
 });
