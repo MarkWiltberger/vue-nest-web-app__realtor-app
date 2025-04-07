83d5b924ea807d261faa28b06d5fd5243b96bccd
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-04-07 08:48:45 -0400

    add test for the controller getHomes method for correct construction of filter

--- backend/src/home/home.controller.spec.ts
+++ backend/src/home/home.controller.spec.ts
@@ -5,6 +5,7 @@ import { PrismaService } from '../prisma/prisma.service';
 
 describe('HomeController', () => {
   let controller: HomeController;
+  let homeService: HomeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,9 +22,21 @@ describe('HomeController', () => {
     }).compile();
 
     controller = module.get<HomeController>(HomeController);
+    homeService = module.get<HomeService>(HomeService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getHomes', () => {
+    it('should construct filter object correctly', async () => {
+      const mockGetHomes = jest.fn().mockReturnValue([]);
+      jest.spyOn(homeService, 'getHomes').mockImplementation(mockGetHomes);
+      await controller.getHomes('Toronto', '1500000');
+
+      expect(mockGetHomes).toBeCalledWith({
+        city: 'Toronto',
+        price: {
+          gte: 1500000,
+        },
+      });
+    });
   });
 });
